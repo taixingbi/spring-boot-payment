aedd0ec73144cfe01ff8311d999e7765c660a544
taixingbi <bitaihang@gmail.com>
2019-06-21 13:52:49 -0400

    sanbox

--- src/main/java/com/example/demo/dao/payment/Module.java
+++ src/main/java/com/example/demo/dao/payment/Module.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Module {
     private static final Logger logger = LogManager.getLogger(Module.class);
-    boolean live= true;//live : sandbox
+    boolean live= false;//live : sandbox
     ErrorHandling errorHandling= new ErrorHandling();
 
     public JsonObject paypalJson( String resquestStr) {
